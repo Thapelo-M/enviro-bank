299fde36cd2475cdb9a9ddc17abfd6a9d1278ccc
Thapelo <keomothokoa@gmail.com>
2021-11-30 21:21:29 +0200

    final commit

--- src/com/enviro/assessment/grad001/keoikantsemothokoa/SystemDB.java
+++ src/com/enviro/assessment/grad001/keoikantsemothokoa/SystemDB.java
@@ -27,25 +27,27 @@ public class SystemDB {
 		//pre-populating accounts 
 		savingsOne.id = 101;
 		savingsOne.accountNum = "1";
-		savingsOne.balance = new BigDecimal(100);
+		savingsOne.balance = new BigDecimal(2000);
 		
 		savingsTwo.id = 102;
 		savingsTwo.accountNum = "2";
-		savingsTwo.balance = new BigDecimal(1000);
+		savingsTwo.balance = new BigDecimal(5000);
 		
 		currentOne.id = 103;
 		currentOne.accountNum = "3";
 		currentOne.balance = new BigDecimal(1000);
 		currentOne.overdraft = new BigDecimal(10000);
 		
-		currentOne.id = 104;
-		currentOne.accountNum = "4";
-		currentOne.balance = new BigDecimal(1000);
-		currentOne.overdraft = new BigDecimal(10000);
+		currentTwo.id = 104;
+		currentTwo.accountNum = "4";
+		currentTwo.balance = new BigDecimal(-5000);
+		currentTwo.overdraft = new BigDecimal(20000);
 		
 		//Testing accounts against withdraw method
 		savingsOne.withdraw("1", new BigDecimal(1500));
-		currentOne.withdraw("3", new BigDecimal(1500));
+		currentOne.withdraw("3", new BigDecimal(1000));
+		savingsTwo.withdraw("2", new BigDecimal(2500.50));
+		currentTwo.withdraw("4", new BigDecimal(5000));
 		
 	}
 }
